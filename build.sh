4e7ecf89e5b159591834b35ab416da37c64850ae
Tomoeko <61221651+Tomoeko@users.noreply.github.com>
2026-07-09 19:09:59 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,10 @@ cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DGAME_ID=000002
 cmake --build build -j"$NCPU"
 cd ..
 
+# Copy compiled module to default Dolphin User path search directory
+DEST_DIR="$HOME/Library/Application Support/Dolphin/StaticRecompModules"
+mkdir -p "$DEST_DIR"
+cp module-template/build/g000002_recomp.dylib "$DEST_DIR/g000002_recomp.dylib"
+echo "=== Copied module to default user directory: $DEST_DIR ==="
+
 echo "=== Build Complete! ==="
